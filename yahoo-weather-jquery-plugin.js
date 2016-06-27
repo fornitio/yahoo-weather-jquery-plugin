28081c1fc0bcf4504f294c1c3be0d17695e504cc
Alexander Fursov <fornitio@gmail.com>
2016-06-27 20:43:37 +0300

    refactored

--- yahoo-weather-jquery-plugin.js
+++ yahoo-weather-jquery-plugin.js
@@ -14,39 +14,52 @@
 		});
 		location
 			.then((location)=>{
-				const query = `select * from weather.forecast where woeid in (SELECT woeid FROM geo.places WHERE text="(${location.coords.latitude},${location.coords.longitude})")`;  
+				const query = `
+					select * from weather.forecast where woeid in (
+						SELECT woeid FROM geo.places 
+						WHERE text="(${location.coords.latitude},${location.coords.longitude})"
+					)
+				`;  
 				const uri = `https://query.yahooapis.com/v1/public/yql?q=${query}&format=json`;
-				fetch(uri, config)
-					.then(res => res.json())
-					.then(res => {
-						def.city = res.query.results.channel.location.city;
-						def.country = res.query.results.channel.location.country;
-						def.date = new Date(res.query.created);
-						def.tHigh = fahrenheitToCelsius(res.query.results.channel.item.forecast[0].high);
-						def.tLow = fahrenheitToCelsius(res.query.results.channel.item.forecast[0].low);
-						def.condition = res.query.results.channel.item.condition.code;
-						def.temp = fahrenheitToCelsius(res.query.results.channel.item.condition.temp);
-						const template = `
-							<div class="btn btn-default">
-								<i class="wi wi-yahoo-${def.condition}">
-									&nbsp;${def.temp}&deg;C  ${def.city}, ${def.country}
-								</i>
-								<div>
-									${def.date.getDate()}/${def.date.getMonth()}, 
-									${def.date.getHours()}:${
-										(def.date.getMinutes()<10) ? '0' + def.date.getMinutes() : def.date.getMinutes()
-									} 
-								</div>
-								<div>
-									&downarrow;t&deg;=${def.tLow}&deg;C,&nbsp; 		
-									&uparrow;t&deg;=${def.tHigh}&deg;C		
-								</div>
-								<a href="https://www.yahoo.com/?ilc=401" target="_blank"> <img src="https://poweredby.yahoo.com/purple.png" width="134" height="29"/> </a>
-							</div>
-							`;
-						this.append(template);
-			});
-		}).catch((err)=>{console.log(err)});
+				return fetch(uri, config)
+			})
+			.then(res => res.json())
+			.then(res => {
+				const item = res.query.results.channel.item;
+				def.city = res.query.results.channel.location.city;
+				def.country = res.query.results.channel.location.country;
+				def.date = new Date(res.query.created);
+				def.tHigh = fahrenheitToCelsius(item.forecast[0].high);
+				def.tLow = fahrenheitToCelsius(item.forecast[0].low);
+				def.condition = item.condition.code;
+				def.temp = fahrenheitToCelsius(item.condition.temp);
+				def.template = template;
+				this.append(def.template());
+			})
+			.catch((err)=>{console.log(err)});
 		return this;
+
+		function template() {
+			return `
+				<div class="btn btn-default">
+					<i class="wi wi-yahoo-${this.condition}">
+						&nbsp;${this.temp}&deg;C  ${this.city}, ${this.country}
+					</i>
+					<div>
+						${this.date.getDate()}/${this.date.getMonth()}, 
+						${this.date.getHours()}:${
+							(this.date.getMinutes()<10) ? '0' + this.date.getMinutes() : this.date.getMinutes()
+						} 
+					</div>
+					<div>
+						&downarrow;t&deg;=${this.tLow}&deg;C,&nbsp; 		
+						&uparrow;t&deg;=${this.tHigh}&deg;C		
+					</div>
+					<a href="https://www.yahoo.com/?ilc=401" target="_blank"> <!--Yahoo obligates to insert this-->
+						<img src="https://poweredby.yahoo.com/purple.png" width="134" height="29"/> 
+					</a>
+				</div>
+			`;
+		};
 	}
 })(jQuery)
